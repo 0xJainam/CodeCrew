366dff4291d900022745fcbc2c791cf6cc2d0233
0xJainam <jainamsonigra@gmail.com>
2026-06-19 23:52:33 +0530

    configure production cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,7 +11,14 @@ app.use(helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
 }));
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            process.env.FRONTEND_URL,
+        ],
+        credentials: true,
+    })
+);
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
